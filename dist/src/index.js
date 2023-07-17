b2f056250578e8ab8f2ceba4201a355ed3b1eab3
srgyCheese <sergey.sirtsilin@gmail.com>
2023-07-18 00:01:35 +0400

    add cors

--- dist/src/index.js
+++ dist/src/index.js
@@ -5,15 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const standalone_1 = require("@trpc/server/adapters/standalone");
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const trpc_1 = require("./trpc");
 const appRouter = (0, trpc_1.router)({
-    test: trpc_1.publicProcedure.query(() => 'test nice'),
-    profile: trpc_1.publicProcedure.query(() => {
-    }),
+    test: trpc_1.publicProcedure.query(() => "test nice"),
+    profile: trpc_1.publicProcedure.query(() => { }),
 });
 const server = (0, standalone_1.createHTTPServer)({
     router: appRouter,
+    middleware: (0, cors_1.default)()
 });
 server.listen(8080);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
-import { createHTTPServer } from '@trpc/server/adapters/standalone'
+import { createHTTPServer } from "@trpc/server/adapters/standalone"
+import cors from "cors"
 
 dotenv.config()
 
@@ -7,16 +8,15 @@ import * as trpc from "@trpc/server"
 import { publicProcedure, router } from "./trpc"
 
 const appRouter = router({
-  test: publicProcedure.query(() => 'test nice'),
-  profile: publicProcedure.query(() => {
-
-  }),
+  test: publicProcedure.query(() => "test nice"),
+  profile: publicProcedure.query(() => {}),
 })
 
 const server = createHTTPServer({
   router: appRouter,
+  middleware: cors()
 })
- 
+
 server.listen(8080)
 
 export type AppRouter = typeof appRouter
