a52aaf018457b46c91afccae47eea58333765361
srgyCheese <sergey.sirtsilin@gmail.com>
2023-07-17 23:46:29 +0400

    .

--- dist/src/index.js
+++ dist/src/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
+const standalone_1 = require("@trpc/server/adapters/standalone");
 dotenv_1.default.config();
 const trpc_1 = require("./trpc");
 const appRouter = (0, trpc_1.router)({
@@ -11,4 +12,8 @@ const appRouter = (0, trpc_1.router)({
     profile: trpc_1.publicProcedure.query(() => {
     }),
 });
+const server = (0, standalone_1.createHTTPServer)({
+    router: appRouter,
+});
+server.listen(8080);
 //# sourceMappingURL=index.js.map
