db6f7b6c9d5e1d3143a6e3c95f82bc3bb260161d
srgyCheese <sergey.sirtsilin@gmail.com>
2023-07-17 21:14:31 +0400

    test

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.get("/", (_req: Request, res: Response) => {
 })
 
 app.get("/ping", (_req: Request, res: Response) => {
-  return res.send("pong 🏓")
+  return res.send("pong 🏓!")
 })
 
 app.listen(port, () => {
