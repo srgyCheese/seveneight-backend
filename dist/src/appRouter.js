7dd2f57b36ab24d5b331894274b78c7d1eb8be59
srgyCheese <sergey.sirtsilin@gmail.com>
2023-07-19 00:51:42 +0400

    add comment request

--- dist/src/appRouter.js
+++ dist/src/appRouter.js
@@ -1,11 +1,48 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.appRouter = void 0;
 const trpc_1 = require("./trpc");
+const zod_1 = __importDefault(require("zod"));
+const client_1 = require("../prisma/client");
+const server_1 = require("@trpc/server");
 exports.appRouter = (0, trpc_1.router)({
     test: trpc_1.publicProcedure.query(() => "test nice"),
     profile: trpc_1.authProcedure.query(({ ctx }) => {
         return ctx.user;
     }),
+    addComment: trpc_1.authProcedure
+        .input(zod_1.default.object({
+        to: zod_1.default.string(),
+    }))
+        .mutation(({ ctx, input }) => __awaiter(void 0, void 0, void 0, function* () {
+        const toUser = yield client_1.prisma.user.findFirst({
+            where: {
+                vk_id: input.to,
+            },
+        });
+        if (!toUser) {
+            throw new server_1.TRPCError({ code: "BAD_REQUEST" });
+        }
+        const comment = yield client_1.prisma.userComment.create({
+            data: {
+                fromUserId: ctx.user.id,
+                toUserId: toUser.id,
+                commentTemplateId: "0",
+            },
+        });
+        return { comment };
+    })),
 });
 //# sourceMappingURL=appRouter.js.map
--- src/appRouter.ts
+++ src/appRouter.ts
@@ -1,10 +1,40 @@
 import { authProcedure, publicProcedure, router } from "./trpc"
+import z from "zod"
+import { prisma } from "../prisma/client"
+import { TRPCError } from "@trpc/server"
 
 export const appRouter = router({
   test: publicProcedure.query(() => "test nice"),
   profile: authProcedure.query(({ ctx }) => {
     return ctx.user
   }),
+  addComment: authProcedure
+    .input(
+      z.object({
+        to: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const toUser = await prisma.user.findFirst({
+        where: {
+          vk_id: input.to,
+        },
+      })
+
+      if (!toUser) {
+        throw new TRPCError({ code: "BAD_REQUEST" })
+      }
+
+      const comment = await prisma.userComment.create({
+        data: {
+          fromUserId: ctx.user.id,
+          toUserId: toUser.id,
+          commentTemplateId: "0",
+        },
+      })
+
+      return { comment }
+    }),
 })
 
 export type AppRouter = typeof appRouter
